c450779db7f1fa730c40d2430af0bedadcd48f69
PanidaRumriankit <panida.rum@ku.th>
2024-09-16 14:01:59 +0700

    Added type hints

--- scorecard.py
+++ scorecard.py
@@ -24,18 +24,18 @@ class Scorecard:
 
     def __init__(self):
         """Iniiialize a new Scorecard."""
-        self.scores = []
+        self.scores: list[float] = []
 
-    def add_score(self, score):
+    def add_score(self, score: float):
         """Add a score to the Scorecard."""
         self.scores.append(score)
 
-    def average(self):
+    def average(self) -> float:
         """Return the average of all scores, 0 if no scores."""
         return sum(self.scores)/max(1,len(self.scores))
 
 
-def print_scores(score_card):
+def print_scores(score_card: Scorecard):
     """Print statistics for the scorecard and the actual scores."""
 
     # What changes to Scorecard are needed in order to make this code work?
@@ -46,12 +46,12 @@ def print_scores(score_card):
         print(score)
 
 
-def ordinal(num):
+def ordinal(num) -> str:
     """Return the ordinal value of an integer; works for numbers up to 20.
 
     For examples: ordinal(1) is '1st', ordinal(2) is '2nd'.
     """
-    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    suffixes: dict[int: str] = {1: "st", 2: "nd", 3: "rd"}
     return str(num) + suffixes.get(num, "th")
 
 
